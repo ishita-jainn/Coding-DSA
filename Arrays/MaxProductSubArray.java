b81add7881ad50bfcdf8faaf1e49f19be03a720c
Ishita <ishitaj232@gmail.com>
2023-05-12 14:30:07 +0530

    Add new files

---
+++ Arrays/MaxProductSubArray.java
@@ -0,0 +1,22 @@
+public class MaxProductSubArray {
+    public int maxProduct(int[] nums) {
+        int ans = nums[0];
+        int n= nums.length;
+        int l=1;
+        int r=1;
+        for(int i=0;i<n;i++){
+            
+            l=  l == 0?1:l;
+            r= r==0?1:r;
+
+            l *=nums[i];
+            r *=nums[n-1-i];
+
+            ans = Math.max(ans,Math.max(l,r));
+        }
+        return ans;
+
+    }
+
+    
+}
